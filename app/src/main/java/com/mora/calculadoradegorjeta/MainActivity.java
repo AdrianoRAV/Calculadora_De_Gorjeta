887306fda8b1218ac2116bf3497f356a0d55654d
adriano <adrianoskte@gmail.com>
2020-10-16 16:21:03 -0300

    project part 2

--- app/src/main/java/com/mora/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/mora/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,50 @@ package com.mora.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editvalor;
+    private TextView texPorcentagem;
+    private TextView textGorjeta;
+    private  TextView textTotal;
+    private SeekBar seekBarGorjeta;
+    private double porcentagem = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editvalor  = findViewById(R.id.editValor);
+        texPorcentagem  = findViewById(R.id.textPorcentagem);
+        textGorjeta  = findViewById(R.id.textGorjeta);
+        textTotal  = findViewById(R.id.editValor);
+         seekBarGorjeta = findViewById(R.id.seekGorjeta);
+
+         //adicionar listener seekBar
+         seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+             @Override
+             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+
+
+             }
+
+             @Override
+             public void onStartTrackingTouch(SeekBar seekBar) {
+
+             }
+
+             @Override
+             public void onStopTrackingTouch(SeekBar seekBar) {
+
+             }
+         });
+
     }
 }
